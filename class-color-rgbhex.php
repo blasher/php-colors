c08e9cb801a376169f1aa0398ee581c083e9bc6a
Brian Lasher <me@brianlasher.com>
2016-11-14 11:51:22 -0600

    initial commit of rgb class and rgbhex class and modifications to test.php to test the new classes

---
+++ class-color-rgbhex.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************************
+ *                                                                         *
+ *   class-color-rgbhex.php                                                *
+ *                                                                         *
+ *   Copyright (C) 2011 by Brian Lasher                                    *
+ *   me@brianlasher.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+require_once( dirname(__FILE__) . '/class-color.php' );
+
+if (!class_exists("RGBHexColor"))
+{	class RGBHexColor extends Color
+	{
+		// PHP 5 Constructor
+		function __construct($hex_r, $hex_g, $hex_b)
+		{	
+			$r = hexdec($hex_r);
+			$g = hexdec($hex_g);
+			$b = hexdec($hex_b);
+
+			parent::__construct( $r, $b, $g );
+		}
+	}
+}
--- test.php
+++ test.php
@@ -1,26 +1,23 @@
 <?php
 
 require_once( dirname(__FILE__) . '/class-color.php' );
+require_once( dirname(__FILE__) . '/class-color-rgb.php' );
+require_once( dirname(__FILE__) . '/class-color-rgbhex.php' );
+require_once( dirname(__FILE__) . '/class-color-rgbhexstring.php' );
 
-$color1 = new Color( 255, 255, 255 );
+$color1 = new Color( 255, 63, 63 );
 echo 'COLOR 1' ."\n";
 echo '===================================' . "\n";
 $color1->dump();
 
 
-$color2 = new Color( 32, 32, 32 );
+$color2 = new RGBColor( 63, 255, 63 );
 echo 'COLOR 2' ."\n";
 echo '===================================' . "\n";
 $color2->dump();
 
 
-$color3 = new Color( 255, 63, 63 );
+$color3 = new RGBHexColor( '80', '40', 'ff' );
 echo 'COLOR 3' ."\n";
 echo '===================================' . "\n";
 $color3->dump();
-
-
-$color4 = new Color( 63, 255, 63 );
-echo 'COLOR 4' ."\n";
-echo '===================================' . "\n";
-$color4->dump();
